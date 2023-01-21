159eeb7a46e1ecd44b96e430ecba30b302a9fdc8
Kamyar Ghasemlou <github@kamy.me>
2023-01-21 10:24:20 +0100

    initial implementation

---
+++ datadog.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+
+from datadog_api_client import ApiClient, Configuration
+from datadog_api_client.v2.api.metrics_api import MetricsApi
+from datadog_api_client.v2.model.metric_intake_type import MetricIntakeType
+from datadog_api_client.v2.model.metric_payload import MetricPayload
+from datadog_api_client.v2.model.metric_point import MetricPoint
+from datadog_api_client.v2.model.metric_resource import MetricResource
+from datadog_api_client.v2.model.metric_series import MetricSeries
+
+
+def get_metric(**kwargs):
+    return MetricPayload(
+        series=[
+            MetricSeries(
+                metric="slack.emoji.reaction",
+                type=MetricIntakeType.COUNT,
+                points=[
+                    MetricPoint(
+                        timestamp=int(datetime.now().timestamp()),
+                        value=1,
+                    ),
+                ],
+                resources=[
+                    MetricResource(
+                        type=key,
+                        name=value,
+                    ) for key, value in kwargs.items()
+                ],
+            ),
+        ],
+    )
+
+def publish_emoji_metric(**kwargs):
+    configuration = Configuration()
+    with ApiClient(configuration) as api_client:
+        api_instance = MetricsApi(api_client)
+        response = api_instance.submit_metrics(body=get_metric(**kwargs))
+        print(response)
